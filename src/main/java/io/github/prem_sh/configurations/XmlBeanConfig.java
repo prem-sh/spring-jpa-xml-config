7e853f01808d3ad8dc54c04580cb8625ad2ba232
Premkumar <premkumaralaguraj@gmail.com>
2022-10-25 10:09:28 +0530

    initial commit

---
+++ src/main/java/io/github/prem_sh/configurations/XmlBeanConfig.java
@@ -0,0 +1,10 @@
+package io.github.prem_sh.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:beans.xml")
+public class XmlBeanConfig {
+
+}
